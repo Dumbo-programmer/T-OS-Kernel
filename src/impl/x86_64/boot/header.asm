98548367f19e9e1434c2cb25f38facb72e23136d
Dumbo-programmer <72726301+Dumbo-programmer@users.noreply.github.com>
2021-11-30 15:55:10 +0600

    Update header.asm

--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -2,11 +2,11 @@ section .multiboot_header
 header_start:
 	
 	dd 0xe85250d6 ; multiboot2
-	
-	dd 0 
-	
+	; architecture
+	dd 0 ; protected mode i386
+	; header length
 	dd header_end - header_start
-
+	; checksum
 	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
 
 	; end tag

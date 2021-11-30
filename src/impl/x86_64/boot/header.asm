6412f03b4918ec7f15d90d09985449118a3eb295
Dumbo-programmer <72726301+Dumbo-programmer@users.noreply.github.com>
2021-11-30 15:42:24 +0600

    Create header.asm

---
+++ src/impl/x86_64/boot/header.asm
@@ -0,0 +1,16 @@
+section .multiboot_header
+header_start:
+	
+	dd 0xe85250d6 ; multiboot2
+	
+	dd 0 
+	
+	dd header_end - header_start
+
+	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+	; end tag
+	dw 0
+	dw 0
+	dd 8
+header_end:
